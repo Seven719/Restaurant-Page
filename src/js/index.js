2b8deeb333b2e45c012933f52977bd081a549eec
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-05-13 10:06:41 +0100

    Added js files

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const Home = document.getElementById("home")
+const About = document.getElementById("about")
+const Menu = document.getElementById("menu")
+
+Home.addEventListener('click', () => {
+    console.log("home")
+})
+
+About.addEventListener('click', () => {
+    console.log("About")
+})
+
+Menu.addEventListener('click', () => {
+    console.log("Menu")
+})
